06d9368067351f7a1d1df960403cf9363472e236
Tabasom Aryamanesh <tabasom.aryamanesh@capitalontap.com>
2017-10-18 09:01:40 +0100

    cart redux test

--- src/components/pages/cart.test.js
+++ src/components/pages/cart.test.js
@@ -8,19 +8,43 @@ import { configure } from "enzyme";
 configure({ adapter: new Adapter() });
 
 describe("cart shallow with store", () => {
-  const ReactComponent = () => <Cart />;
-  describe("state", () => {
-    it("works", () => {
+  // This is my cart component
+  const CartComponent = () => <Cart />;
+
+  // create mock store -  test mapstatetoprops works
+  describe("Shallow rendering with mocked store", () => {
+    it("Cart works", () => {
       const expectedState = "expectedState";
       const mapStateToProps = state => ({
         state
       });
-      const ConnectedComponent = connect(mapStateToProps)(ReactComponent);
+      const ConnectedComponent = connect(mapStateToProps)(CartComponent);
       const component = shallowWithStore(
         <ConnectedComponent />,
         createMockStore(expectedState)
       );
       expect(component.props().state).toBe(expectedState);
+    });
+  });
+
+  describe("Testing dispatch/action with mocked store", () => {
+    it("Cart works", () => {
+      const action = {
+        type: "type"
+      };
+      const mapDispatchToProps = dispatch => ({
+        dispatchProp() {
+          dispatch(action);
+        }
+      });
+      const store = createMockStore();
+
+      const ConnectedComponent = connect(undefined, mapDispatchToProps)(
+        CartComponent
+      );
+      const component = shallowWithStore(<ConnectedComponent />, store);
+      component.props().dispatchProp();
+      expect(store.isActionDispatched(action)).toBe(true);
     });
   });
 });
